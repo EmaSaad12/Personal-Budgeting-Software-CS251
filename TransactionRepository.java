70f86b7b58f416a04338fc05008e76ebde7a906a
samabasiony882-star <samasameh882@gmail.com>
2026-05-07 01:40:12 +0300

    added some edits on Eman's part

--- TransactionRepository.java
+++ TransactionRepository.java
@@ -1,25 +1,65 @@
+package com.mycompany.a2.us6and7;
 
-import java.io.FileWriter;
+import java.io.*;
+import java.util.*;
 
 public class TransactionRepository {
 
     public void save(Transaction t) {
+        try (FileWriter fw = new FileWriter("transactions.txt", true)) {
+            
+            fw.write(t.getId() + "," + t.getType() + "," + t.getAmount() + "," + 
+                     t.getCategory() + "," + t.getUserId() + "," + t.getDate() + "\n");
+        } catch (Exception e) { e.printStackTrace(); }
+    }
+    
+        // ✅ الجديد بقى
+    public ArrayList<Transaction> loadTransactions() {
+
+        ArrayList<Transaction> list = new ArrayList<>();
+
         try {
-            FileWriter fw = new FileWriter("transactions.txt", true);
+            File file = new File("transactions.txt");
 
-            fw.write(
-                t.getId() + "," +
-                t.getType() + "," +
-                t.getAmount() + "," +
-                t.getCategory() + "," +
-                t.getUserId() + ","+
-                t.getDate() + "\n"
-            );
+            if (!file.exists()) return list;
 
-            fw.close();
+            Scanner sc = new Scanner(file);
+
+            while (sc.hasNextLine()) {
+
+                String line = sc.nextLine();
+                String[] data = line.split(",");
+                
+                if(data.length>=6){
+                                String type = data[1];
+                double amount = Double.parseDouble(data[2]);
+                String category = data[3];
+                String userId = data[4];
+                String date = data[5];
+
+                Transaction t = new Transaction(type, amount, category, date, userId);
+                java.lang.reflect.Field idField = Transaction.class.getDeclaredField("id");
+                idField.setAccessible(true);
+                idField.set(t, data[0]);
+
+                list.add(t);
+                
+                }
+
+            }
+
+            sc.close();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        return list;
     }
+
+
+
 }
+
+
+
